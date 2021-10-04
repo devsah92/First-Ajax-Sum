c9c4be010eebdef0c18924fbab8f40141598266e
devsah92 <90085170+devsah92@users.noreply.github.com>
2021-10-04 16:49:54 +0530

    ajx

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Math(int nr1, int nr2)
+        {
+            var result1 = nr1 + nr2;
+            var data = new { status = "ok", result = "result1"};
+
+            return Json(data,JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
